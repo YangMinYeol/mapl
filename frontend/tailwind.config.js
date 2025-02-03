d2aed4de049974c8659b7275eb0fe154b8c02873
YangMinYeol <sheep0125@naver.com>
2025-02-03 19:02:44 +0900

    feat: 프로젝트 기본 폰트 및 색상 변경

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        "deep-green": "#173836",
+        "mapl-black": "#333",
+        "mapl-slate": "#ddd",
+      },
       fontFamily: {
-        sans: ["Pretendard Variable", "Pretendard", "sans-serif"],
-      }
+        sans: [
+          "Pretendard",
+          "Noto Sans KR",
+          "Malgun Gothic",
+          "AppleGothic",
+          "Dotum",
+          "sans-serif",
+        ],
+      },
     },
   },
   plugins: [],
-}
-
+};
