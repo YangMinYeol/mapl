d569607c4bd7fac77c88435400b00ad56c52a44b
YangMinYeol <sheep0125@naver.com>
2025-02-03 19:04:35 +0900

    feat: Logo 디자인 변경 및 색상 추가 Logo 색상을 고급진 느낌을 주기 위해 딥그린 색상으로 변경

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,7 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import LogoImage from "../assets/images/logo.svg";
+import LogoWhiteImage from "../assets/images/logo_white.svg";
 
-export default function Logo({ height }) {
+export default function Logo({ height = "h-auto", useWhiteLogo = false }) {
   const navigate = useNavigate();
 
   function navigateToHome() {
@@ -10,9 +11,9 @@ export default function Logo({ height }) {
 
   return (
     <img
-      src={LogoImage}
+      src={useWhiteLogo ? LogoWhiteImage : LogoImage}
       alt="Mapl Logo"
-      className={`cursor-pointer ${height || "h-auto"}`} // height가 없으면 기본값 제공
+      className={`cursor-pointer ${height}`}
       onClick={navigateToHome}
     />
   );
