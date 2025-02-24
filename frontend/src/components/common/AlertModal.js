502dc7807b84e3c4b6461c68da6d9542199882b2
YangMinYeol <myyang@gdnps.com>
2025-02-24 16:28:01 +0900

    feat: update AlertModal alert로 표현하던 메시지를 AlertModal로 사용하게끔 컴포넌트 변경

---
+++ frontend/src/components/common/AlertModal.js
@@ -0,0 +1,38 @@
+import Modal from "react-modal";
+import { useEffect } from "react";
+
+export default function AlertModal({ isOpen, message, onClose }) {
+  useEffect(() => {
+    Modal.setAppElement("#root");
+  }, []);
+  const customStyle = {
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.4)",
+    },
+    content: {
+      padding: 0,
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      transform: "translate(-50%, -50%)",
+      borderRadius: "10px",
+    },
+  };
+
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyle}>
+      <div className="p-6 text-base font-semibold text-mapl-black">
+        <span>{message}</span>
+      </div>
+      <div>
+        <button
+          onClick={onClose}
+          className="w-full p-3 font-semibold text-center border text-deep-green"
+        >
+          확인
+        </button>
+      </div>
+    </Modal>
+  );
+}
