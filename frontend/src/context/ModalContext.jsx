b354baa00c6c3a4e5cae7451a3e6adf50918c1a2
YangMinYeol <myyang@gdnps.com>
2025-03-10 16:31:35 +0900

    feat: .env api 설정 변경, closeModal 함수 추가

--- frontend/src/context/ModalContext.jsx
+++ frontend/src/context/ModalContext.jsx
@@ -9,15 +9,24 @@ export function useModal() {
 export function ModalProvider({ children }) {
   const [modalOpen, setModalOpen] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
+  const [onClose, setOnClose] = useState(null);
 
-  function openModal(message) {
+  function openModal(message, onCloseCallback = null) {
     setModalMessage(message);
     setModalOpen(true);
+    setOnClose(() => onCloseCallback);
+  }
+
+  function closeModal() {
+    if (onClose) {
+      onClose();
+    }
+    setModalOpen(false);
   }
 
   return (
     <ModalContext.Provider
-      value={{ modalOpen, modalMessage, setModalOpen, openModal }}
+      value={{ modalOpen, modalMessage, setModalOpen, openModal, closeModal }}
     >
       {children}
     </ModalContext.Provider>
