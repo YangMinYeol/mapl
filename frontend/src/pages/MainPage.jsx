a6710d0cb6665be8865035ddf4b4227f8c69bde1
YangMinYeol <sheep0125@naver.com>
2025-03-12 16:33:39 +0900

    feat: date 유틸 생성 YYYYMMDD 포멧 추가

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,16 +1,21 @@
 import Calendar from "../components/calendar/Calendar";
 import Dashboard from "../components/dashboard/Dashboard";
+import { formatDateYYYYMMDD } from "../util/dateUtil";
 import { useState } from "react";
 
 export default function MainPage() {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const date = new Date();
+  const [selectedDate, setSelectedDate] = useState(formatDateYYYYMMDD(date));
+  function handleSetSelectedDate(date) {
+    setSelectedDate(formatDateYYYYMMDD(date));
+  }
   return (
     <div>
       <div className="flex flex-wrap h-[900px]">
         <div className="w-full h-full calendar-container md:w-[70%]">
           <Calendar
             selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
+            setSelectedDate={handleSetSelectedDate}
           />
         </div>
         <div className="w-full h-full detail-container md:w-[30%]">
