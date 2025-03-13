1d253b31e3b151324a7a00fc936e9a0d6387da3b
YangMinYeol <myyang@gdnps.com>
2025-03-13 16:37:38 +0900

    feat: 메모 삭제 기능 추가 아이콘을 눌러 메모 삭제 기능 추가

--- backend/controller/memoController.js
+++ backend/controller/memoController.js
@@ -35,4 +35,19 @@ async function addMemo(req, res) {
   }
 }
 
-module.exports = { getMemo, addMemo };
+// 메모 삭제
+async function deleteMemo(req, res) {
+  const { memoId } = req.body;
+
+  try {
+    await memoModel.deleteMemo(memoId);
+    return res.status(200).json({ message: "메모가 삭제되었습니다." });
+  } catch (error) {
+    console.error("메모 삭제 오류", error);
+    return res
+      .status(500)
+      .json({ message: "메모 삭제 중 서버에서 문제가 발생하였습니다." });
+  }
+}
+
+module.exports = { getMemo, addMemo, deleteMemo };
--- frontend/src/components/dashboard/MemoItem.jsx
+++ frontend/src/components/dashboard/MemoItem.jsx
@@ -17,18 +17,19 @@ function IconButton({ icon, onClick }) {
   );
 }
 
-export default function MemoItem({ text }) {
+export default function MemoItem({ memo, onDelete }) {
   return (
     <div className="flex items-center px-1 py-1 hover:bg-gray-50 group">
       <div className="flex justify-center w-1/20 ">
+        {/* Complete */}
         <IconButton icon={faSquare} />
       </div>
-      <div className="w-17/20">{text}</div>
+      <div className="w-17/20">{memo.content}</div>
       <div className="grid grid-cols-2 opacity-0 w-2/20 group-hover:opacity-100">
         {/* Edit */}
         <IconButton icon={faPenToSquare} />
         {/* Delete */}
-        <IconButton icon={faTrashCan} />
+        <IconButton icon={faTrashCan} onClick={() => onDelete(memo.id)} />
       </div>
     </div>
   );
