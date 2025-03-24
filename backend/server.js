0e58356839fa5f95209edccf07e0ab5caef5e263
YangMinYeol <sheep0125@naver.com>
2025-03-24 14:23:50 +0900

    feat: cookie-parser 라이브러리 추가

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ const cors = require("cors");
 const userRoute = require("./route/userRoute");
 const memoRoute = require("./route/memoRoute");
 const periodRoute = require("./route/periodRoute");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 // 라우트 설정
 app.use("/api/user", userRoute);
