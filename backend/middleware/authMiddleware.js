6646f5f2c559856795aab864bb60c052f7fff028
YangMinYeol <myyang@gdnps.com>
2025-03-25 10:39:56 +0900

    feat: fetch시 인증 처리 추가 로그인이 필요한 서비스에대한 인증 요구

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,25 +2,23 @@ const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
   const token = req.headers.authorization?.split(" ")[1];
-  let errorMessage = "인증이 필요합니다.";
 
   if (!token) {
-    return res.status(401).json({ message: errorMessage });
+    return res.status(401).json({ message: "인증이 필요합니다." });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    req.user = decoded;
+    req.user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     next();
   } catch (error) {
-    errorMessage = "토큰이 유효하지 않습니다.";
-    if (error.name === "TokenExpiredError") {
-      errorMessage = "토큰이 만료되었습니다";
-      console.error(errorMessage, error);
-      return res.status(401).json({ message: errorMessage });
-    }
+    const isTokenExpired = error.name === "TokenExpiredError";
+    const statusCode = isTokenExpired ? 401 : 403;
+    const errorMessage = isTokenExpired
+      ? "토큰이 만료되었습니다."
+      : "토큰이 유효하지 않습니다.";
+
     console.error(errorMessage, error);
-    return res.status(403).json({ message: errorMessage });
+    return res.status(statusCode).json({ message: errorMessage });
   }
 }
 
--- backend/route/memoRoute.js
+++ backend/route/memoRoute.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const memoController = require("../controller/memoController");
+const verifyToken = require("../middleware/authMiddleware");
 
 // 메모 목록
-router.get("/", memoController.getMemo);
+router.get("/", verifyToken, memoController.getMemo);
 
 // 메모 추가
-router.post("/", memoController.addMemo);
+router.post("/", verifyToken, memoController.addMemo);
 
 // 메모 삭제
-router.delete("/", memoController.deleteMemo);
+router.delete("/", verifyToken, memoController.deleteMemo);
 
 // 메모 완료 상태 변경
-router.post("/complete", memoController.toggleMemoCompletion);
+router.post("/complete", verifyToken, memoController.toggleMemoCompletion);
 
 module.exports = router;
