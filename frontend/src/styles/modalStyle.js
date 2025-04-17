1630b096e21008d65306fab43e34f4be73354fb4
YangMinYeol <sheep0125@naver.com>
2025-04-17 09:03:24 +0900

    feat: modal 스타일 코드 분리

---
+++ frontend/src/styles/modalStyle.js
@@ -0,0 +1,15 @@
+export const baseModalStyle = {
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.4)",
+  },
+  content: {
+    padding: "5px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    transform: "translate(-50%, -50%)",
+    borderRadius: "10px",
+    minWidth: "150px",
+  },
+};
