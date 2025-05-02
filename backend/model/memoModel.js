41642aed17acddad4ac3b10a344297278d097b98
YangMinYeol <sheep0125@naver.com>
2025-05-02 14:04:53 +0900

    feat: 메모 정렬 기준 변경 기존 sort_order로 정렬하던 메모 정렬 기준을 새로운 기준 확립후 사용

--- backend/model/memoModel.js
+++ backend/model/memoModel.js
@@ -13,7 +13,8 @@ async function getMemo(userId, selectedDate) {
     SELECT * 
     FROM memo 
     WHERE user_id = $1 
-      AND ((start_date <= $2 AND end_date >= $2) OR period_id = 5)
+    AND ((start_date <= $2 AND end_date >= $2) OR period_id = 5)
+    ORDER BY memo.start_date, memo.end_date DESC, memo.created_at, memo.id
   `;
   return db.query(query, [userId, selectedDate]);
 }
@@ -31,17 +32,20 @@ async function getCalendarMemo(userId, currentDate) {
       memo.user_id,
       memo.content,
       memo.start_date,
+      memo.end_date,
+      memo.start_time,
+      memo.end_time,
       memo.period_id,
-      memo.sort_order,
       memo.completed,
       memo.allday,
       memo.color_id,
+      memo.created_at,
       color.hex AS color_hex
     FROM memo
     JOIN color ON memo.color_id = color.id
     WHERE memo.user_id = $1
     AND memo.start_date BETWEEN $2 AND $3
-    ORDER BY memo.period_id, memo.start_date, memo.sort_order
+    ORDER BY memo.start_date, memo.end_date DESC, memo.created_at, memo.id
   `;
 
   return db.query(query, [userId, startDate, endDate]);
@@ -69,13 +73,10 @@ async function addMemo(memos) {
         colorId = 10,
       } = memoData;
 
-      // sort_order 계산
-      const newSortOrder = await getNewSortOrder(userId, startDate, periodId);
-
       // 1. 메모를 추가하면서 link는 임시로 NULL로 설정
       const insertQuery = `
-        INSERT INTO memo (user_id, content, start_date, end_date,start_time, end_time, allday, sort_order, period_id, color_id, is_linked, link )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, NULL)
+        INSERT INTO memo (user_id, content, start_date, end_date,start_time, end_time, allday, period_id, color_id, is_linked, link )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, NULL)
         RETURNING id;
       `;
 
@@ -87,7 +88,6 @@ async function addMemo(memos) {
         startTime,
         endTime,
         allDay,
-        newSortOrder,
         periodId,
         colorId,
         isLinked,
@@ -115,31 +115,6 @@ async function addMemo(memos) {
     console.error("메모 추가 중 오류 발생:", error);
     throw new Error("메모 추가 실패: " + error.message);
   }
-}
-
-async function getNewSortOrder(userId, startDate, periodId) {
-  let query;
-  let params;
-
-  // 버킷리스트인 경우
-  if (startDate === null) {
-    query = `
-      SELECT COALESCE(MAX(sort_order), 0) + 1 AS new_sort_order
-      FROM memo
-      WHERE user_id = $1 AND period_id = $2 AND start_date IS NULL;
-    `;
-    params = [userId, periodId];
-  } else {
-    query = `
-      SELECT COALESCE(MAX(sort_order), 0) + 1 AS new_sort_order
-      FROM memo
-      WHERE user_id = $1 AND start_date = $2 AND period_id = $3;
-    `;
-    params = [userId, startDate, periodId];
-  }
-
-  const result = await db.query(query, params);
-  return result.rows[0].new_sort_order;
 }
 
 // 메모 삭제
