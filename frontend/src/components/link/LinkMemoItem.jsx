8179fd8e01cd775a711451664a382914e53610e4
YangMinYeol <myyang@gdnps.com>
2025-05-16 16:31:03 +0900

    feat: 링크 확인 페이지 날짜 시간 표기

--- frontend/src/components/link/LinkMemoItem.jsx
+++ frontend/src/components/link/LinkMemoItem.jsx
@@ -1,14 +1,40 @@
 import { formatDateYYYYMMDD } from "../../util/dateUtil";
+import { formatTimeTo12Hour } from "../../util/timeUtil";
 
 export default function LinkMemoItem({ memo }) {
-  return (
-    <div className="flex items-center px-1 py-1 hover:bg-gray-50 group">
-      <div className="flex items-center"></div>
+  const { periodName, startDate, endDate, startTime, endTime } = memo;
 
-      <div>
-        {`${formatDateYYYYMMDD(memo.startDate)} ~ ${formatDateYYYYMMDD(
-          memo.endDate
-        )}`}
+  const formattedStartDate = formatDateYYYYMMDD(startDate);
+  const formattedEndDate = formatDateYYYYMMDD(endDate);
+
+  const isBucketList = periodName === "Bucket List";
+
+  return (
+    <div className="flex items-center py-1.5 my-1 border rounded hover:bg-gray-50 group border-mapl-slate">
+      <div className="flex w-full">
+        {isBucketList ? (
+          <div className="flex items-center justify-center w-full font-semibold text-center">
+            {periodName}
+          </div>
+        ) : (
+          <>
+            <div className="flex items-center justify-center w-1/5 font-semibold">
+              {periodName}
+            </div>
+            <div className="flex flex-col w-4/5 text-sm">
+              <div className="flex">
+                <div className="w-2/12">From</div>
+                <div className="w-5/12">: {formattedStartDate}</div>
+                <div className="w-5/12">{formatTimeTo12Hour(startTime)}</div>
+              </div>
+              <div className="flex">
+                <div className="w-2/12">To</div>
+                <div className="w-5/12">: {formattedEndDate}</div>
+                <div className="w-5/12">{formatTimeTo12Hour(endTime)}</div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
