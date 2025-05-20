c21d65f62c8c4b72524025a1cf6169b8c8a4cbf3
YangMinYeol <sheep0125@naver.com>
2025-05-20 14:57:32 +0900

    feat: Footer 높이 조절

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="py-4 text-white bg-deep-green">
+    <footer className="text-white py-7 bg-deep-green">
       <div className="text-center">
-        <p className="mt-2">
+        <p className="my-1">
           제작: 양민열 <span className="mx-2">|</span> 이메일:
           devmy0125@gmail.com
         </p>
