8ad2cb790344609f7b4e5f55a633147e7c97f290
YangMinYeol <myyang@gdnps.com>
2025-06-19 13:45:06 +0900

    feat: 창사이즈 변경에 따른 UI 디테일 개선

--- frontend/src/components/dashboard/PeriodSelector.jsx
+++ frontend/src/components/dashboard/PeriodSelector.jsx
@@ -6,20 +6,23 @@ export default function PeriodSelector({
   return (
     <div className="h-[40px] font-medium border-y-1 border-mapl-slate">
       <nav>
-        <ul className="flex justify-center py-2 period-nav-list">
+        <div className="flex justify-center py-2 period-nav-list">
           {periods.map((period) => (
-            <li
+            <div
               key={period.id}
-              className={`${
-                period.id === selectedPeriod.id &&
-                " text-green-900 underline decoration-green-900"
-              } flex items-center justify-center w-full cursor-pointer nav-item hover:text-green-900 hover:underline hover:decoration-green-900`}
+              className={`flex-1 min-w-0 flex items-center justify-center cursor-pointer nav-item hover:text-green-900 hover:underline hover:decoration-green-900 ${
+                period.id === selectedPeriod.id
+                  ? "text-green-900 underline decoration-green-900"
+                  : ""
+              }`}
               onClick={() => setSelectedPeriod(period)}
             >
-              <span>{period.name}</span>
-            </li>
+              <span className=" whitespace-nowrap">
+                {period.name}
+              </span>
+            </div>
           ))}
-        </ul>
+        </div>
       </nav>
     </div>
   );
