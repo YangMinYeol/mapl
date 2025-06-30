cfa288bb753dc78ddfe8792be97d704f9415d230
YangMinYeol <myyang@gdnps.com>
2025-06-30 18:20:48 +0900

    feat: 회원가입시 자산 초기값 설정

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -13,13 +13,38 @@ function checkUserIdOrEmailExist(field, value) {
 }
 
 // 회원가입
-function createUser(userData) {
+async function createUser(userData) {
   const { userId, name, password, email, zipcode, address, detailAddress } =
     userData;
-  return db.query(
-    "INSERT INTO users (user_id, name, password, email, zipcode, address, detail_address) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
-    [userId, name, password, email, zipcode, address, detailAddress]
-  );
+
+  const client = await db.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    // 1. 유저 생성 및 id 반환
+    const userResult = await client.query(
+      `INSERT INTO users (user_id, name, password, email, zipcode, address, detail_address)
+     VALUES ($1, $2, $3, $4, $5, $6, $7)
+     RETURNING id`,
+      [userId, name, password, email, zipcode, address, detailAddress]
+    );
+
+    const createdUserId = userResult.rows[0].id;
+
+    // 2. asset 테이블에 초기값 0 삽입
+    await client.query(`INSERT INTO asset (user_id, balance) VALUES ($1, $2)`, [
+      createdUserId,
+      0,
+    ]);
+    await client.query("COMMIT");
+    return createdUserId;
+  } catch (error) {
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
 }
 
 module.exports = { getUserInfo, checkUserIdOrEmailExist, createUser };
