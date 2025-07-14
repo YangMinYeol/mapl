d70114b6e40646a62b8d9cd2b5b2f4dfd0d179e2
YangMinYeol <myyang@gdnps.com>
2025-07-14 15:53:47 +0900

    feat: 달력에 나타내는 메모와 가계부에대한 코드 리팩토링

--- frontend/src/components/Calendar/MemoTag.jsx
+++ frontend/src/components/Calendar/MemoTag.jsx
@@ -2,19 +2,19 @@ import { isSameDay } from "date-fns";
 import { TAG_TYPE } from "../../constants/tag";
 import Tooltip from "../common/Tooltip";
 
-function getMemoTagClass(memo, date) {
-  if (!memo) return "";
+function getMemoTagClass(tag, date) {
+  if (!tag) return "";
 
-  if (memo.type === TAG_TYPE.MORE) {
+  if (tag.type === TAG_TYPE.MORE) {
     return "mx-1 text-gray-500 rounded";
   }
 
-  if (memo.type === TAG_TYPE.HOLIDAY) {
+  if (tag.type === TAG_TYPE.HOLIDAY) {
     return "mx-1 text-white rounded";
   }
 
-  const isStart = memo.startDate && isSameDay(date, new Date(memo.startDate));
-  const isEnd = memo.endDate && isSameDay(date, new Date(memo.endDate));
+  const isStart = tag.startDate && isSameDay(date, new Date(tag.startDate));
+  const isEnd = tag.endDate && isSameDay(date, new Date(tag.endDate));
 
   return [
     "text-white",
@@ -25,37 +25,36 @@ function getMemoTagClass(memo, date) {
     .join(" ");
 }
 
-export default function MemoTag({ memo, date, onClick }) {
+export default function MemoTag({ tag, date, onClick }) {
   const shouldShow =
-    memo?.content &&
-    (memo.type === TAG_TYPE.MORE ||
-      memo.type === TAG_TYPE.HOLIDAY ||
-      isSameDay(date, new Date(memo.startDate)) ||
+    tag?.content &&
+    (tag.type === TAG_TYPE.MORE ||
+      tag.type === TAG_TYPE.HOLIDAY ||
+      isSameDay(date, new Date(tag.startDate)) ||
       date.getDay() === 0);
 
   const handleClick = (e) => {
-    if (memo) {
+    if (tag) {
       e.stopPropagation();
-      onClick(memo);
+      onClick(tag);
     }
   };
 
   const memoTag = (
     <div
-      key={memo?.id}
-      className={`px-1 h-5 truncate ${getMemoTagClass(memo, date)} ${
-        memo && "hover:cursor-pointer hover:font-bold"
+      key={tag?.id}
+      className={`px-1 h-5 truncate ${getMemoTagClass(tag, date)} ${
+        tag && "hover:cursor-pointer hover:font-bold"
       }`}
-      style={memo ? { backgroundColor: memo.colorHex } : {}}
+      style={tag ? { backgroundColor: tag.colorHex } : {}}
       onClick={handleClick}
     >
-      {shouldShow ? memo.content : null}
+      {shouldShow ? tag.content : null}
     </div>
   );
 
-  return memo &&
-    (memo.type === TAG_TYPE.DAILY || memo.type === TAG_TYPE.RANGE) ? (
-    <Tooltip memo={memo}>{memoTag}</Tooltip>
+  return tag && (tag.type === TAG_TYPE.DAILY || tag.type === TAG_TYPE.RANGE) ? (
+    <Tooltip memo={tag}>{memoTag}</Tooltip>
   ) : (
     memoTag
   );
