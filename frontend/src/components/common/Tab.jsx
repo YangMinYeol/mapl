7830870812a9b24533180488df1353ae9be15269
YangMinYeol <myyang@gdnps.com>
2025-07-17 02:32:51 +0900

    feat: Tab을 다양한곳에서 다양한 크기로 사용할 수 있게 size prop 추가

--- frontend/src/components/common/Tab.jsx
+++ frontend/src/components/common/Tab.jsx
@@ -1,11 +1,12 @@
-export default function Tab({ options, selected, onSelect }) {
+export default function Tab({ options, selected, onSelect, size = "sm" }) {
+  const sizeClass = size === "lg" ? "text-base px-2" : "px-1";
   return (
     <div className="space-x-1">
       {options.map((option) => (
         <button
           key={option.value}
           onClick={() => onSelect(option.value)}
-          className={`px-1 border border-mapl-slate cursor-pointer rounded-md
+          className={`${sizeClass} border border-mapl-slate cursor-pointer rounded-md
             ${
               selected === option.value
                 ? "bg-deep-green text-white"
