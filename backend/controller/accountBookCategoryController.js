7e7fd8323aa915ccdf2248a84b8f7ef7812136a5
YangMinYeol <myyang@gdnps.com>
2025-07-20 16:16:10 +0900

    feat: 가계부 카테고리 삭제 코드 추가

--- backend/controller/accountBookCategoryController.js
+++ backend/controller/accountBookCategoryController.js
@@ -35,7 +35,24 @@ async function addAccountBookCategory(req, res) {
   }
 }
 
+// 가계부 카테고리 삭제
+async function deleteAccountBookCategory(req, res) {
+  const { categoryId } = req.params;
+  try {
+    await accountBookCategoryModel.deleteAccountBookCategory(categoryId);
+    return res
+      .status(200)
+      .json({ message: "가계부 카테고리가 삭제되었습니다." });
+  } catch (error) {
+    console.error("가계부 카테고리 삭제 오류", error);
+    return res.status(500).json({
+      message: "가계부 카테고리 삭제 중 서버에서 문제가 발생하였습니다.",
+    });
+  }
+}
+
 module.exports = {
   getCategoriesByUser,
   addAccountBookCategory,
+  deleteAccountBookCategory,
 };
