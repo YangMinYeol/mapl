e63484b58c056fef1a095748af8ad7465f29d945
YangMinYeol <sheep0125@naver.com>
2025-07-25 09:08:13 +0900

    feat: errorType 유틸 파일을 추가해 상수로 관리

--- frontend/src/util/error.js
+++ frontend/src/util/error.js
@@ -1,5 +1,7 @@
+import { ERROR_TYPE } from "./errorType";
+
 export class CustomError extends Error {
-  constructor(message, type = "GENERAL_ERROR") {
+  constructor(message, type = ERROR_TYPE.GENERAL_ERROR) {
     super(message);
     this.type = type;
     this.name = this.constructor.name;
@@ -8,6 +10,6 @@ export class CustomError extends Error {
 
 export class LoginExpiredError extends CustomError {
   constructor(message = "로그인이 만료되었습니다. 다시 로그인해 주세요.") {
-    super(message, "LOGIN_EXPIRED");
+    super(message, ERROR_TYPE.LOGIN_EXPIRED);
   }
 }
